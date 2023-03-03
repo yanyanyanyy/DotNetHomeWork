448c3bf0ef96ef30f9729adf7c4b7a36f4ba399a
yan <he321yan@163.com>
2023-03-03 18:33:50 +0800

    第一次作业

---
+++ assignment1/project1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个操作数:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个操作数:");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入操作符:");
+            int op = Console.Read();
+
+            if (op == '+')
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == '-')
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == '*')
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == '/') 
+            { 
+                Console.WriteLine(num1 / num2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment1/project2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(this.textBox1.Text.ToString());
+            double num2 = double.Parse(this.textBox2.Text.ToString());
+            if (this.comboBox1.SelectedIndex == 0)
+            {
+                this.label2.Text = "" + (num1 + num2);
+            }
+            else if (this.comboBox1.SelectedIndex == 1)
+            {
+                this.label2.Text = "" + (num1 - num2);
+            }
+            else if (this.comboBox1.SelectedIndex == 2)
+            {
+                this.label2.Text = "" + (num1 * num2);
+            }
+            else if (this.comboBox1.SelectedIndex == 3)
+            {
+                this.label2.Text = "" + (num1 / num2);
+            }
+            else
+            {
+                this.label2.Text = "" + (num1 + num2);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
